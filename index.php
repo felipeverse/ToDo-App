506a0823b709d91883f1088f0a697c271c91b5e0
felipeverse <felipealvesrrodrigues@outlook.com>
2022-05-09 17:20:12 -0400

    implement sections

--- index.php
+++ index.php
@@ -3,6 +3,12 @@ require __DIR__ . '/tasks/tasks.php';
 
 $tasks = getTasks();
 
+$tasksToDo = [];
+$tasksDone = [];
+foreach ($tasks as $id => $task) {
+    $task['completed'] ? $tasksDone[$id] = $task : $tasksToDo[$id] = $task;
+}
+
 if ($_GET['errors']) {
     $errors = json_decode($_GET['errors'], true);
 }
@@ -28,7 +34,18 @@ if ($_GET['errors']) {
             </form>
         </div>
 
-        <?php foreach ($tasks as $id => $task): ?>
+        <!-- Tasks To Do -->
+        <ul class="list-group list-group-flush">
+            <li class="list-group-item"> 
+                <i class="bi bi-list-task"></i><b> To Do</b>
+            </li>
+        </ul>
+
+        <?php if (count($tasksToDo) == 0): ?>
+            <p class="text-center">Nothing to see here.</p>
+        <?php endif; ?>
+
+        <?php foreach ($tasksToDo as $id => $task): ?>
             <div class="card my-2 mx-2">
                 <div class="card-body d-flex flex-row align-items-center">
                     <form class="form-check" action="/tasks/change_status.php" method="POST" style="display: inline-block">
@@ -38,7 +55,34 @@ if ($_GET['errors']) {
                     <span class="card-text flex-grow-1 mx-2"><?php echo $task['text'] ?></span>
                     <div class="">
                         <a class="text-success ms-3" href="#"><i class="bi bi-pencil-fill"></i></a>
+                        <!-- Button trigger delete modal -->
+                        <a class="text-danger ms-3 deleteButton" data-toggle="modal" data-target="#deleteModal" data-id="<?php echo $id?>" href="#"><i class="bi bi-trash2-fill"></i></a>
+                    </div>
+                </div>
+            </div>
+        <?php endforeach; ?>
 
+        <!-- Done Tasks -->
+        <ul class="list-group list-group-flush">
+            <li class="list-group-item">
+            <i class="bi bi-check2-all"></i><b> Done</b>
+            </li>
+        </ul>
+
+        <?php if (count($tasksDone) == 0): ?>
+            <p class="text-center">Nothing to see here.</p>
+        <?php endif; ?>
+
+        <?php foreach ($tasksDone as $id => $task): ?>
+            <div class="card my-2 mx-2">
+                <div class="card-body d-flex flex-row align-items-center">
+                    <form class="form-check" action="/tasks/change_status.php" method="POST" style="display: inline-block">
+                        <input type="hidden" name="id" value="<?php echo $id ?>">
+                        <input class="form-check-input" style="border-radius: 50%;" type="checkbox" name="" id="" name="todo_check" <?php echo $task['completed'] ? 'checked':'' ?> >
+                    </form>
+                    <span class="card-text flex-grow-1 mx-2"><del><?php echo $task['text'] ?></del></span>
+                    <div class="">
+                        <a class="text-success ms-3" href="#"><i class="bi bi-pencil-fill"></i></a>
                         <!-- Button trigger delete modal -->
                         <a class="text-danger ms-3 deleteButton" data-toggle="modal" data-target="#deleteModal" data-id="<?php echo $id?>" href="#"><i class="bi bi-trash2-fill"></i></a>
                     
@@ -46,6 +90,7 @@ if ($_GET['errors']) {
                 </div>
             </div>
         <?php endforeach; ?>
+
     </div>
             
     <!-- Scripts -->
